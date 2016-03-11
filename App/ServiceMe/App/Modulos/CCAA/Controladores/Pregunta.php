04367b95dbe339b2500e6c47136f7d60859fc951
Alejandro Montenegro <alejo_fix@HOTMAIL.COM>
2016-03-11 17:40:09 -0500

    usuarios CONTROL CALIDAD
    Actualización tablas de usuarios y CC
    y Proceso  CCAA/Pregunta/Index/?

--- App/ServiceMe/App/Modulos/CCAA/Controladores/Pregunta.php
+++ App/ServiceMe/App/Modulos/CCAA/Controladores/Pregunta.php
@@ -1,11 +1,12 @@
 <?php
-
+	
 	class Pregunta extends Controlador {
 		
-		function __Construct (){
+		
+		function __Construct() {
 			parent::__Construct();
 		}
-
+		
 		public function Index($tipo = false) {
 			if(is_bool($tipo) == false):
 				$this->validarTipo($tipo);
@@ -18,31 +19,84 @@
 		
 		private function validarTipo($tipo = false) {
 			if(is_numeric($tipo) == true):
-				$this->existenciaTipoPregunta($tipo);
+				$this->existenciaTipo($tipo);
 			else:
 				echo '
-				No hay Formulario Valido ...
+				No hay Formulario Valido de Preguntas  ...
 				';
 			endif;
 		}
 		
-		private function existenciaTipoPregunta($tipo = false) {
-			$validar = $this->Modelo->existenciaTipoPregunta($tipo);
+		private function existenciaTipo($tipo = false) {
+			$validar = $this->Modelo->existenciaTipo($tipo);
 			
 			if($validar['CANTIDAD'] == 1):
 				$this->mostrarPlantilla($tipo);
 			else:
 				echo '
-				Formulario Inexistente ...
+				Formulario Inexistente ...	
 				';
 			endif;
 		}
-
-		private function mostrarPlantilla () {
+		
+		private function mostrarPlantilla($tipo = false) {
 			$pl = new NeuralPlantillasTwig(APP);
+			$pl->Parametro('listado', $this->Modelo->listadoTipo($tipo));
 			$pl->Parametro('tipo', $tipo);
-			$pl->Parametro('listado', $this->Modelo->listadoTipoPregunta($tipo));
+			$pl->Parametro('validacionForm', $this->validacionForm());
+			$pl->Parametro('listadoPreguntas', $this->Modelo->listadoPreguntas());
 			echo $pl->MostrarPlantilla('Pregunta', 'MostrarPlantilla', $tipo.'.html');
 		}
 		
+		private function validacionForm() {
+			$Val = new NeuralJQueryFormularioValidacion(true, true, true);
+			$Val->Numero('CUENTA', 'El Aviso debe Ser Numérico');
+			$Val->CantMaxCaracteres('CUENTA', 10, 'Debe ingresar max 10 Números');
+			$Val->Requerido('CUENTA', 'Debe Ingresar Número Cta Suscriptor.');
+			$Val->ControlEnvio(
+				NeuralJQueryAjaxConstructor::TipoDatos('html')
+                            ->TipoEnvio('POST')
+                            ->Datos('#Formularios')
+                            ->URL(NeuralRutasApp::RutaUrlAppModulo('CCAA', 'Pregunta', 'ProcesarIndex'))
+                      		->FinalizadoEnvio('$("#respuestaDiv").html(Respuesta);')
+			);
+			return $Val->Constructor('Formularios');
+		}
+		
+		
+		public function ProcesarIndex() {
+			if(isset($_POST) == true AND AppValidar::PeticionAjax() == true):
+				$this->ProcesarIndexVacio();
+			else:
+				echo '
+				No resuelve Petición Ajax ...
+				';
+			endif;
+		}
+		
+		private function ProcesarIndexVacio() {
+	 	//	Ayudas::print_r($_POST);
+			if(AppValidar::Vacio()->MatrizDatos($_POST) == true):
+				$this->guardarIndex();
+			else:
+			echo '
+				Hay Datos vacíos en el Formulario ...
+				';
+			endif;
+		}
+		
+		private function guardarIndex() {
+			$datos = AppFormato::Espacio()->Mayusculas()->MatrizDatos($_POST);
+			$this->Modelo->GuardarInfo($datos);
+			$this->plantilla();
+			echo '
+			Información almacenada con éxito ... 
+			';
+		}
+		
+		public function plantilla() {
+			echo '
+			!Gracias!, 
+			';
+		}
 	}
--- App/ServiceMe/App/Modulos/CCAA/Modelos/Pregunta_Modelo.php
+++ App/ServiceMe/App/Modulos/CCAA/Modelos/Pregunta_Modelo.php
@@ -1,15 +1,15 @@
 <?php
-
+	
 	class Pregunta_Modelo extends Modelo {
 		
 		private $conexion = false;
 		
 		function __Construct() {
 			parent::__Construct();
-			$this->conexion = NeuralConexionDB::DoctrineDBAL(APPBD);	
+			$this->conexion = NeuralConexionDB::DoctrineDBAL(APPBD);
 		}
 		
-		public function existenciaTipoPregunta($id = false) {
+		public function existenciaTipo($id = false) {
 			$consulta = $this->conexion->prepare('
 			SELECT 
 				COUNT(ID) 
@@ -20,24 +20,65 @@
 			WHERE 
 				ID = ? 
 			AND 
-				ESTADO = ? 
+				ESTADO = ? 			
 			');
 			$consulta->bindValue(1, $id);
 			$consulta->bindValue(2, 1);
 			$consulta->execute();
 			return $consulta->fetch(PDO::FETCH_ASSOC);
 		}
-
-
-		public function listadoTipoPregunta($tipo = false) {
+		
+		public function listadoTipo($tipo = false) {
 			$consulta = $this->conexion->prepare('
-			
-			
+			SELECT 
+				ID, OPCION 
+			FROM 
+				CCAA_PREGUNTA_OPCION 
+			WHERE 
+				TIPO = ? 
+			AND 
+				ESTADO = ? 
 			');
 			$consulta->bindValue(1, $tipo);
 			$consulta->bindValue(2, 1);
 			$consulta->execute();
 			return $consulta->fetchAll(PDO::FETCH_ASSOC);
-		}		
+		}
 		
+		public function GuardarInfo($array = false) {
+			$SQL = new NeuralBDGab($this->conexion, 'CCAA_PREGUNTA');
+			foreach ($array AS $columna => $valor):
+				$SQL->Sentencia($columna, $valor);
+				$SQL->Sentencia('Fecha', date("Y-m-d H:i:s"));
+			endforeach;
+			return $SQL->Insertar();
+		}
+		
+		/**
+		 * CCAA listadoPreguntas()
+		 * Listado de preguntas del formato Preguntas CCAA
+		 * @author alejo_fix
+		 * @return Array
+		 */	
+		 
+		 public function listadoPreguntas($tipo = false){
+		 	$consulta = $this->conexion->prepare('
+ 			SELECT 
+				CCAA_PREGUNTA_OPCION.ID,
+				CCAA_PREGUNTA_OPCION.TIPO,
+				CCAA_PREGUNTA_OPCION.OPCION,
+				CCAA_PREGUNTA_OPCION.ESTADO 
+			FROM 
+				CCAA_PREGUNTA_OPCION 
+			WHERE 
+				ESTADO = ?	
+			AND 
+				TIPO = ?  	
+			 ');
+			$consulta->bindValue(1, 1);
+			$consulta->bindValue(2, $tipo);
+			$consulta->execute();
+			return $consulta->fetchAll(PDO::FETCH_ASSOC);
+		 }
+	 	
 	}
