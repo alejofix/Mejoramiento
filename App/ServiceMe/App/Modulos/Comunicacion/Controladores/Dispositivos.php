babbc597f59958c535257983b34f24504d3aba26
AlejoFix <alejo_fix@HOTMAIL.COM>
2016-11-21 11:16:50 -0500

    add de Vistas modulo ServiceCo para exportar y apk, up de apk y vistas nuevos dispositivos

--- App/ServiceMe/App/Modulos/Comunicacion/Controladores/Dispositivos.php
+++ App/ServiceMe/App/Modulos/Comunicacion/Controladores/Dispositivos.php
@@ -71,4 +71,65 @@
 			echo $plantilla->MostrarPlantilla(implode(DIRECTORY_SEPARATOR, array('Dispositivos', 'AjaxTelevision.html')));
 		}
 		
+		/**
+		 * Dispositivos::PC()
+		 * 
+		 * Carga pantalla Proyeccion PC a TV 
+		 * @return void
+		 */
+		public function PC() {
+			$plantilla = new NeuralPlantillasTwig(APP);
+			$plantilla->Parametro('Titulo', 'Service-Co');
+			$plantilla->Parametro('avisos', array_chunk($this->Modelo->avisos(), 8));
+			echo $plantilla->MostrarPlantilla(implode(DIRECTORY_SEPARATOR, array('Dispositivos', 'PC.html')));
+		}
+		
+		/**
+		 * Dispositivos::AjaxPC()
+		 * 
+		 * Genera la carga de la informacion de las paginas de datos
+		 * que cambian automaticamente con jquery
+		 * @return raw
+		 */
+		public function AjaxPC() {
+			$data = $this->Modelo->avisos();
+			$pagina = (count($data) >= 1) ? array_chunk($data, 8) : array();
+			
+			$plantilla = new NeuralPlantillasTwig(APP);
+			$plantilla->Parametro('cantidad', count($data));
+			$plantilla->Parametro('paginas', count($pagina));
+			$plantilla->Parametro('listado', $pagina);
+			echo $plantilla->MostrarPlantilla(implode(DIRECTORY_SEPARATOR, array('Dispositivos', 'AjaxPC.html')));
+		}
+		
+		/**
+		 * Dispositivos::Movil()
+		 * 
+		 * Genera la lista de avisos para Celular
+		 * @return raw
+		 */
+		public function Movil() {
+			$plantilla = new NeuralPlantillasTwig(APP);
+			$plantilla->Parametro('Titulo', 'Comunicación');
+			$plantilla->Parametro('avisos', $this->Modelo->avisos());
+			echo $plantilla->MostrarPlantilla(implode(DIRECTORY_SEPARATOR, array('Dispositivos', 'Movil.html')));
+		}
+		
+		/**
+		 * Dispositivos::AjaxMovil()
+		 * 
+		 * Genera la validacion de existencia peticion ajax
+		 * @return void
+		 */
+		public function AjaxMovil() {
+			if(AppValidar::PeticionAjax() == true):
+				$plantilla = new NeuralPlantillasTwig(APP);
+				$plantilla->Parametro('avisos', $this->Modelo->avisos());
+				echo $plantilla->MostrarPlantilla(implode(DIRECTORY_SEPARATOR, array('Dispositivos', 'AjaxMovil.html')));
+			else:
+				echo 'No es posible cargar la información';
+			endif;
+		}
+		
+		
 	}
